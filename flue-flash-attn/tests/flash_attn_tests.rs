e8f63d992175d0a65b8ae526c51dc88bbb750caf
Eric Buehler <ericlbuehler@gmail.com>
2025-03-30 19:03:59 -0400

    Use it in flux model

--- flue-flash-attn/tests/flash_attn_tests.rs
+++ flue-flash-attn/tests/flash_attn_tests.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use candle::{DType, Device, IndexOp, Tensor, D};
+use candle_core::{DType, Device, IndexOp, Tensor, D};
 
 fn to_vec3_round(t: Tensor, digits: i32) -> Result<Vec<Vec<Vec<f32>>>> {
     let b = 10f32.powi(digits);
