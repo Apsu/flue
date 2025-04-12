9c984ccf74e1bacc4f86f2139a503a4f6b39c84a
Eve Callicoat <eve@lambdal.com>
2025-04-12 23:39:48 +0000

    Rustfmt

--- flue-flash-attn-v2/src/lib.rs
+++ flue-flash-attn-v2/src/lib.rs
@@ -144,8 +144,7 @@ impl FlashAttn {
 
         let elem_count = out_shape.elem_count();
         let dst = unsafe { dev.alloc::<T>(elem_count) }?;
-        let softmax_lse = dev
-            .alloc_zeros::<f32>(b_sz * 128 * num_heads * seqlen_q)?;
+        let softmax_lse = dev.alloc_zeros::<f32>(b_sz * 128 * num_heads * seqlen_q)?;
 
         let is_bf16 = if is_bf16 { 1 } else { 0 };
 
@@ -610,8 +609,7 @@ impl FlashAttnVarLen {
 
         let elem_count = out_shape.elem_count();
         let dst = unsafe { dev.alloc::<T>(elem_count) }?;
-        let softmax_lse = dev
-            .alloc_zeros::<f32>(batch_size * num_heads * self.max_seqlen_q)?;
+        let softmax_lse = dev.alloc_zeros::<f32>(batch_size * num_heads * self.max_seqlen_q)?;
 
         let is_bf16 = if is_bf16 { 1 } else { 0 };
 
